dd2bf53d5199cfb29f9e9e8461fa90fe55e715f1
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2021-01-27 23:06:16 -0600

    basic scripts done

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -12,41 +12,56 @@ public class PlayerInventory : MonoBehaviour
     private void Start()
     {
         PlayerIngredients = new List<GameObject>();
-
     }
 
+    /// <summary>
+    ///Handles changes to the inventory
+    ///<para name = "action">Action to be done in the inventory</para>
+    ///<para name = "actionIngredient">The ingredient which will suffer changes</para>
+    /// </summary>
     public void UpdateInventory( string action , GameObject actionIngredient )
     {
         switch( action)
         {
             case "Add":
+                AddItem(action, actionIngredient);
                 break;
             case "Remove":
+                RemoveItem(actionIngredient);
                 break;
         }
     }
 
-
-    private void AddItem( GameObject actionIngredient )
+    /// <summary>
+    ///Increases the quantity of an ingredient or creates it if it does not exists
+    ///<para name = "action">Action to be done in the inventory</para>
+    ///<para name = "actionIngredient">The ingredient which will suffer changes</para>
+    /// </summary>
+    private void AddItem( string action , GameObject actionIngredient )
     {
         bool ingredientExists =PlayerIngredients.Contains( actionIngredient );
         if( ingredientExists)
         {
-            PlayerIngredients[PlayerIngredients.IndexOf(actionIngredient)].GetComponent<InventoryIngredient>().ModifyQuantity("Add", 1);
+            PlayerIngredients[ PlayerIngredients.IndexOf( actionIngredient ) ].GetComponent< InventoryIngredient >().ModifyQuantity( action , 1 );
         }
         else
         {
-            PlayerIngredients.Add(actionIngredient);
+            PlayerIngredients.Add( actionIngredient );
         }
        
     }
 
-    private void RemoveItem(GameObject actionIngredient)
+    /// <summary>
+    ///Removes an item from the inventory.
+    ///<para name = "action">Action to be done in the inventory</para>
+    ///<para name = "actionIngredient">The ingredient which will suffer changes</para>
+    /// </summary>
+    private void RemoveItem( GameObject actionIngredient )
     {
-        bool ingredientExists = PlayerIngredients.Contains(actionIngredient);
-        if (ingredientExists)
+        bool ingredientExists = PlayerIngredients.Contains( actionIngredient );
+        if( ingredientExists )
         {
-            PlayerIngredients.Remove(actionIngredient);
+            PlayerIngredients.Remove( actionIngredient );
         }
     }
 
