66a561112cbde57687f3e4b2ddd9f926111aa9ef
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2021-01-27 22:00:00 -0600

    created branch Bernardo

---
+++ Assets/Scripts/CraftingRecipe.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingRecipe : MonoBehaviour
+{
+    public GameObject FirstIngredient;
+    public GameObject SecondIngredient;
+    public GameObject Result;
+}
---
+++ Assets/Scripts/GameIngredient.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameIngredient : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string Name;
+    public string Description;
+    public int HarvestTime;
+    public int HarvestAmount;
+}
